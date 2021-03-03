71eaa4b2d18e257fc1e7e669089355084fedc3a4
Guilherme Gonzalez Vieira <guilherme.vieira@visionaespacial.com.br>
2021-03-03 18:12:37 -0300

    Updates

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.all('/', function(req, res, next) {
+router.all('/', function (req, res, next) {
   res.render('home');
 });
 
-router.all('/download', function(req, res, next) {
-  var path = 'public/curriculo/GuilhermeGonzalezCurriculoC.docx'
+router.all('/download', function (req, res, next) {
+  var path = 'public/curriculo/GuilhermeCurriculo.pdf'
   res.download(path);
 });
 
